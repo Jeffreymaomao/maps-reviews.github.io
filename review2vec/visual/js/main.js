3dfb90af42ccd752a4eef063414e15f01ca579f3
Chang Mao Yang <jeffrey0613mao@gmail.com>
2024-06-17 18:24:49 +0800

    clusters

--- review2vec/visual/js/main.js
+++ review2vec/visual/js/main.js
@@ -4,24 +4,54 @@ window.addEventListener("load", (e) => {
     window.visualvector2 = visualvector2;
 });
 
+/*
+{
+    "author":...
+    "comment":...
+    "star":5,
+    "PCA":[-3.1391160488,0.0767028704],
+    "cluster2":0,
+    "cluster3":0,
+    "cluster4":0,
+    "cluster5":0,
+    "anomaly-0.01":1,
+    "anomaly-0.05":1,
+    "anomaly-0.1":1,
+    "anomaly-0.15":1,
+    "anomaly-0.2":1}
+*/
+
 class VisualVector2 {
     constructor(domParent) {
         // this.maxNum = 1000;
         this.maxNum = undefined;
         this.dom = {};
+        this.n_cluster = 2; // 2,3,4,5
+        this.n_cmap = 0;
+        this.n_background = 1;
         this.dom.parent = domParent;
+        const _backgrounds = [250, 240, 100, 50, 20, 0];
         this.style = {
             padding: 40,
-            background: '#f0f0f0',
+            backgrounds: _backgrounds.concat(_backgrounds.slice(1, -1).reverse()).map(v=>`rgb(${v},${v},${v})`),
             point: {
                 border: '0.1px',
-                borderColor: '#ccc',
-                colors: ['#E7ADAC', '#A2886D', '#A6BAAF', '#5B7493', '#4A475C'],
-                // colors: ["#ff0000", "#00ff00", "#0000ff", "#ff00ff", "#00ffff"],
-            }
+                borderColor: '#bbb'
+            },
+            cmaps: [
+                ['#E7ADAC', '#A2886D', '#A6BAAF', '#5B7493', '#4A475C'],
+                ["#ff0000", "#0ff000", "#000ff0", "#ff00ff", "#00ffff"],
+                ['#FF6347', '#2E8B57', '#4682B4', '#708090', '#6A5ACD'],
+                ['#20B2AA', '#778899', '#B0C4DE', '#BA55D3', '#9370DB'],
+                ['#2E8B57', '#4682B4', '#708090', '#6A5ACD', '#FF6347'],
+                ['#20B2AA', '#778899', '#B0C4DE', '#BA55D3', '#9370DB'],
+                ['#8B0000', '#FF4500', '#32CD32', '#00CED1', '#1E90FF'],
+                ['#FFD700', '#FF69B4', '#4B0082', '#8A2BE2', '#00FA9A'],
+                ['#DC143C', '#00FF7F', '#FF8C00', '#8B008B', '#00BFFF']
+            ] 
         };
 
-        fetch("./pca_clustered_reviews.json")
+        fetch("./pca_clustered_reviews-emojis.json")
             .then(response => {
                 return response.json();
             })
@@ -30,6 +60,7 @@ class VisualVector2 {
                 this.initDom();
                 this.renderItems();
                 this.addTooltipListener();
+                this.addKeyListener();
             })
             .catch(e => { console.error(e); })
     }
@@ -38,17 +69,26 @@ class VisualVector2 {
         const authors = data.map(item => item.author);
         const comments = data.map(item => item.comment);
         const stars = data.map(item => item.star);
-        const clusters = data.map(item => item.cluster);
-        const pca2_1 = data.map(item => item.pca2_1);
-        const pca2_2 = data.map(item => item.pca2_2);
-        const vectors = this.zip([pca2_1, pca2_2]);
+        // const pca2_1 = data.map(item => item.pca2_1);
+        // const pca2_2 = data.map(item => item.pca2_2);
+        // const vectors = this.zip([pca2_1, pca2_2]);
+        const vectors = data.map(item => item.PCA);
+        
+        const clusters2 = data.map(item => item.cluster2);
+        const clusters3 = data.map(item => item.cluster3);
+        const clusters4 = data.map(item => item.cluster4);
+        const clusters5 = data.map(item => item.cluster5);
 
+        
         return {
             authors,
             stars,
             comments,
-            clusters,
-            vectors
+            vectors,
+            clusters2,
+            clusters3,
+            clusters4,
+            clusters5,
         };
     }
     zip(arrays) {
@@ -65,7 +105,7 @@ class VisualVector2 {
         this.dom.tooltip = this.createAndAppendElement(this.dom.parent, "div", {
             id: 'tooltip',
         });
-        this.dom.container.style.background = this.style.background;
+        this.dom.container.style.background = this.style.backgrounds[this.n_background];
     }
 
     renderItems() {
@@ -84,15 +124,25 @@ class VisualVector2 {
         const yScale = (y) => (y - yRange[0]) / (yRange[1] - yRange[0]) * (height - 2 * pad) + pad;
 
         const pointStyle = this.style.point;
+        this.dom.points = [];
         this.data.vectors.forEach((vector, i) => {
             const x = xScale(vector[0]);
             const y = yScale(vector[1]);
-            const cluster = this.data.clusters[i]
-            const item = this.createAndAppendElement(container, 'div', {
+            const point = this.createAndAppendElement(container, 'div', {
                 class: `point`,
                 dataset: { index: i },
-                style: `left: ${x}px; top: ${y}px; background-color: ${pointStyle.colors[cluster]}; border: ${pointStyle.border} solid ${pointStyle.borderColor}`
+                style: `left: ${x}px; top: ${y}px; border: ${pointStyle.border} solid ${pointStyle.borderColor}`
             });
+            this.dom.points.push(point);
+        });
+        this.renderItemsColor();
+    }
+
+    renderItemsColor(){
+        const clusters = this.data[`clusters${this.n_cluster}`];
+        this.dom.points.forEach((point, i) => {
+            const cluster = clusters[i];
+            point.style.backgroundColor = this.style.cmaps[this.n_cmap][cluster];
         });
     }
 
@@ -159,6 +209,28 @@ class VisualVector2 {
             starsHTML += '☆';
         }
         return starsHTML;
+    }
+
+    addKeyListener() {
+        window.addEventListener('keydown', (e) => {
+            if(e.code==='Digit2') {
+                this.n_cluster = 2; this.renderItemsColor()
+            } else if (e.code==='Digit3') {
+                this.n_cluster = 3; this.renderItemsColor()
+            } else if (e.code==='Digit4') {
+                this.n_cluster = 4; this.renderItemsColor()
+            } else if (e.code==='Digit5') {
+                this.n_cluster = 5; this.renderItemsColor()
+            } else if (e.code==='KeyC') {
+                this.n_cmap += 1;
+                this.n_cmap = (this.n_cmap)%(this.style.cmaps.length);
+                this.renderItemsColor()
+            } else if  (e.code==='KeyB') {
+                this.n_background += 1;
+                this.n_background = (this.n_background)%(this.style.backgrounds.length);
+                this.dom.container.style.background = this.style.backgrounds[this.n_background];
+            }
+        });
     }
 
     createAndAppendElement(parent, tag, attributes = {}) {
